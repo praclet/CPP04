1fe634f5795aa0e58830d5bf172695fe64c583d5
Philippe Raclet <praclet.tel@gmail.com>
2021-04-07 10:28:40 +0200

    Saving files

--- ex03/headers/MateriaSource.hpp
+++ ex03/headers/MateriaSource.hpp
@@ -6,20 +6,28 @@
 /*   By: praclet <praclet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/05 17:51:35 by praclet           #+#    #+#             */
-/*   Updated: 2021/04/05 17:55:23 by praclet          ###   ########lyon.fr   */
+/*   Updated: 2021/04/07 10:27:45 by praclet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MATERIASOURCE_HPP
 # define MATERIASOURCE_HPP
 
+# include <string>
 # include "IMateriaSource.hpp"
+# include "AMateria.hpp"
 
-class MateriaSource
+class MateriaSource : public IMateriaSource
 {
+	protected:
+		static std::size_t const _nbMaxMaterias = 4;
+		AMateria*	_tabMaterias[_nbMaxMaterias];
 	public:
-		virtual ~MateriaSource() {};
-		virtual void learnMateria(AMateria*);
+		MateriaSource();
+		MateriaSource(MateriaSource const & src);
+		virtual ~MateriaSource();
+		virtual MateriaSource & operator=(MateriaSource const & src);
+		virtual void learnMateria(AMateria* element);
 		virtual AMateria* createMateria(std::string const & type);
 };
 
--- ex03/srcs/MateriaSource.cpp
+++ ex03/srcs/MateriaSource.cpp
@@ -6,8 +6,50 @@
 /*   By: praclet <praclet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/05 17:52:48 by praclet           #+#    #+#             */
-/*   Updated: 2021/04/05 17:53:11 by praclet          ###   ########lyon.fr   */
+/*   Updated: 2021/04/07 10:24:04 by praclet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
 #include "MateriaSource.hpp"
+
+MateriaSource::MateriaSource()
+{
+	for (std::size_t i=0;i < _nbMaxMaterias;i++)
+		_tabMaterias[i] = NULL;
+}
+
+MateriaSource::MateriaSource(MateriaSource const & src)
+{
+	for (std::size_t i=0;i < _nbMaxMaterias;i++)
+		_tabMaterias[i] = src._tabMaterias[i];
+}
+
+MateriaSource::~MateriaSource()
+{
+}
+
+MateriaSource & MateriaSource::operator=(MateriaSource const & src)
+{
+	for (std::size_t i=0;i < _nbMaxMaterias;i++)
+		_tabMaterias[i] = src._tabMaterias[i];
+	return (*this);
+}
+
+void MateriaSource::learnMateria(AMateria* element)
+{
+	for (std::size_t i=0;i < _nbMaxMaterias;i++)
+		if (!_tabMaterias[i])
+		{
+			_tabMaterias[i] = element;
+			break;
+		}
+}
+
+AMateria* MateriaSource::createMateria(std::string const & type)
+{
+	for (std::size_t i=0;i < _nbMaxMaterias;i++)
+		if (_tabMaterias[i]->getType() == type)
+			return (_tabMaterias[i]->clone());
+	return (NULL);
+}
